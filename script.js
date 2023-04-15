9f0a105fc8661e8353e2546b589e48dfb7bab2e6
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-15 21:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function firstNonRepeatedCharacter(str) {
+    let charCount = {};
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+    return null;
+}
+
+let inputString = prompt("Enter a string: ");
+let result = firstNonRepeatedCharacter(inputString);
+alert( result);
